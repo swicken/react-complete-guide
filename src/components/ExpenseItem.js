444aefa3fe9e75084e2e4b633be4ab647457bef0
Scott Wicken <78741732+swicken-dotcms@users.noreply.github.com>
2021-09-17 15:47:39 -0400

    doing things

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,20 @@
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+
+function ExpenseItem(props) {
+
+
+
+  return (
+    <div className="expense-item">
+
+      <div className="expense-item__description">
+        <ExpenseDate date={props.date}/>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
