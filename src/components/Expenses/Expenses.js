09b74195d4886a19569286567a67205e82885fc5
Scott Wicken <78741732+swicken-dotcms@users.noreply.github.com>
2021-09-23 16:41:28 -0400

    just a checkpoint I guess

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import ExpenseItem from "./ExpenseItem";
-import Card from "./Card";
+import Card from "../UI/Card";
 import "./Expenses.css";
 
-function Expenses(props) {
+const Expenses = (props) => {
   let expenseList = props.expenses.map((expense) => (
     <ExpenseItem
       title={expense.title}
